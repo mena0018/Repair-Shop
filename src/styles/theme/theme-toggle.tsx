fd78a0617b4b32643adb19aed147484fba4d2b96
Rabie Menad <rabie.menad.partner@decathlon.com>
2024-12-07 22:12:03 +0100

    fix: :lipstick: some UI issues

--- src/styles/theme/theme-toggle.tsx
+++ src/styles/theme/theme-toggle.tsx
@@ -1,10 +1,9 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { MoonIcon, SunIcon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import * as React from 'react';
-
-import { Button } from '@/components/ui/button';
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -15,8 +14,8 @@ export function ThemeToggle() {
 
   return (
     <Button variant='ghost' size='icon' onClick={handleTheme} className='rounded-full'>
-      <SunIcon className='dark:-rotate-90 size-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0' />
-      <MoonIcon className='absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <SunIcon className='dark:-rotate-90 rotate-0 scale-100 transition-all dark:scale-0' />
+      <MoonIcon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
       <span className='sr-only'>Toggle theme</span>
     </Button>
   );
